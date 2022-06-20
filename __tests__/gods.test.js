7c52b4f27f48c6f922318811fd98c7c131f6bdab
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-19 22:13:25 -0700

    succesfully displaying all gods. It took me awhile to understand why my test was failing and after I added data by sql table it became obvious that the test had to match my data exactly. Also, a syntax error and type resolved. yay!!

--- __tests__/gods.test.js
+++ __tests__/gods.test.js
@@ -7,6 +7,31 @@ describe('god routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('get /gods should display a list of gods', async () => {
+    const resp = await request(app).get('/gods');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual([
+      {
+        drink: 'Iced American (no sugar)',
+        id: '1',
+        known: 'Lord of Justice',
+        name: 'Zeus',
+      },
+      {
+        drink: 'Espresso shot',
+        id: '2',
+        known: 'Known as Neptune',
+        name: 'Poseidon',
+      },
+      {
+        drink: 'Hot Oatmilk Latte',
+        id: '3',
+        known: 'Goddess of wisdom, war and the crafts',
+        name: 'Athena',
+      },
+      { drink: 'Iced Mocha', id: '4', known: 'Most Loved God', name: 'Apollo' },
+    ]);
+  });
 
   it(' POST /gods should add new god', async () => {
     const resp = await request(app).post('/gods').send({
--- sql/setup.sql
+++ sql/setup.sql
@@ -8,6 +8,8 @@ CREATE TABLE gods (
     known VARCHAR NOT NULL,
     drink VARCHAR NOT NULL
 );
-INSERT INTO gods (name, known, drink) VALUES (
-    'Zeus', 'Lord of Justice', 'Iced American (no sugar)'
-);
+INSERT INTO gods (name, known, drink) 
+VALUES
+('Zeus', 'Lord of Justice', 'Iced American (no sugar)'), ('Poseidon', 'Known as Neptune', 'Espresso shot'),
+('Athena', 'Goddess of wisdom, war and the crafts', 'Hot Oatmilk Latte'),
+('Apollo', 'Most Loved God', 'Iced Mocha');
