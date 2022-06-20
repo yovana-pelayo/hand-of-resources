9eec8a0d881b4e67f5ef5bde3b9c1cdbb6517d9a
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-19 22:25:39 -0700

    created an update/edit test for my update method

--- __tests__/gods.test.js
+++ __tests__/gods.test.js
@@ -46,6 +46,13 @@ describe('god routes', () => {
     expect(resp.body.drink).toEqual('cold brew');
     expect(resp.body.id).not.toBeUndefined();
   });
+  it('PUT /gods/:id should update/ edit reptile', async () => {
+    const resp = await (
+      await request(app).put('/gods/2')
+    ).setEncoding({ drink: 'Espresso Shots on the Rocks' });
+    expect(resp.status).toEqual(200);
+    expect(resp.body.drink).toEqual('Espresso Shots on the Rocks');
+  });
   afterAll(() => {
     pool.end();
   });
