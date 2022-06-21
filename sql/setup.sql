9ec0ed80f9f83006105cf13c9d31e2d701a0b2d6
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-21 00:25:12 -0700

    able to retrieve all artist by using a getAll method

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,6 +4,7 @@ DROP TABLE if exists gods;
 DROP TABLE if exists sharks;
 DROP TABLE if exists zodiacs;
 DROP TABLE if exists presidents;
+DROP TABLE if exists artists;
 
 CREATE TABLE gods (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -18,8 +19,6 @@ CREATE TABLE sharks (
     species VARCHAR NOT NULL,
     family VARCHAR NOT NULL
 );
-
-
 CREATE TABLE zodiacs ( 
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
@@ -31,6 +30,10 @@ CREATE TABLE presidents (
     start VARCHAR NOT NULL,
     final VARCHAR NOT NULL
 );
+CREATE TABLE artists( id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    genre VARCHAR NOT NULL,
+ hit VARCHAR NOT NULL);
 
 INSERT INTO gods (name, known, drink) 
 VALUES
@@ -40,7 +43,10 @@ VALUES
 
 INSERT INTO sharks (name, species, family) 
 VALUES ('bull shark','Carcharhinus leucas','Carcharhinidae'),('ghost shark','Callorhinchus milii','Carcharhinidae'),('tiger shark','Galeocerdo cuvier','Carcharhinidae'), ('lemon shark','Negaprion brevirostris','Carcharhinidae');
+
 INSERT INTO zodiacs (name, type) VALUES  ('Taurus','Earth'), ('Scorpio', 'Water'), ('Leo', 'Fire'), ('Libra','Air');
 
 INSERT INTO presidents (name, start, final) VALUES 
 ('Biden', '2021', 'Current'), ('Trump', '2017', '2021'), ('Obama', '2009', '2017'), ('Bush', '2001', '2009');
+
+INSERT INTO artists (name, genre, hit) VALUES ( 'Queen', 'ROCK','Bohemian Rhapsody' ), ('Kid Cudi','RAP','Pursuit of Happiness' ), ('Miley Cryus', 'ROCk', 'Midnight Sky');
