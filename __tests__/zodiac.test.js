a037f8d85986c989ab03a6301b549145aa9e5d62
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-20 22:19:23 -0700

    able to insert a new zodiac sign by using the put method

--- __tests__/zodiac.test.js
+++ __tests__/zodiac.test.js
@@ -11,11 +11,37 @@ describe('zodiacs routes', () => {
     const resp = await request(app).get('/zodiacs');
     expect(resp.status).toEqual(200);
     expect(resp.body).toEqual([
-      { id: '1', sign: 'Taurus', type: 'Earth' },
-      { id: '2', sign: 'Scorpio', type: 'Water' },
-      { id: '3', sign: 'Leo', type: 'Fire' },
-      { id: '4', sign: 'Libra', type: 'Air' },
+      { id: '1', name: 'Taurus', type: 'Earth' },
+      { id: '2', name: 'Scorpio', type: 'Water' },
+      { id: '3', name: 'Leo', type: 'Fire' },
+      { id: '4', name: 'Libra', type: 'Air' },
     ]);
+  });
+  it('get /zodiacs/:id should display zodiac detail', async () => {
+    const resp = await request(app).get('/zodiacs/1');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'Taurus',
+      type: 'Earth',
+    });
+  });
+  it('POST /zodiacs should add new zodiac', async () => {
+    const resp = await request(app).post('/zodiacs').send({
+      name: 'Virgo',
+      type: 'Earth',
+    });
+
+    expect(resp.status).toEqual(200);
+    expect(resp.body.name).toEqual('Virgo');
+    expect(resp.body.type).toEqual('Earth');
+    expect(resp.body.id).not.toBeUndefined();
+  });
+  it('PUT /zodiacs/:id should update/ edit zodiac', async () => {
+    const resp = await request(app).put('/zodiacs/4').send({ name: 'Libra!' });
+    console.log('resp', resp.body);
+    expect(resp.status).toEqual(200);
+    expect(resp.body.name).toEqual('Libra!');
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 DROP TABLE if exists gods;
 DROP TABLE if exists sharks;
 DROP TABLE if exists zodiacs;
+
 CREATE TABLE gods (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
@@ -16,11 +17,12 @@ CREATE TABLE sharks (
     species VARCHAR NOT NULL,
     family VARCHAR NOT NULL
 );
- CREATE TABLE zodiacs (
+
+
+CREATE TABLE zodiacs ( 
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   sign VARCHAR NOT NULL,
-   type VARCHAR NOT NULL
-  
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL
 );
 
 INSERT INTO gods (name, known, drink) 
@@ -34,4 +36,6 @@ VALUES('bull shark','Carcharhinus leucas','Carcharhinidae'),
         ('ghost shark','Callorhinchus milii',
         'Carcharhinidae'),
         ('tiger shark','Galeocerdo cuvier','Carcharhinidae'), ('lemon shark','Negaprion brevirostris','Carcharhinidae');
-        INSERT INTO zodiacs (sign, type) VALUES('Taurus','Earth'), ('Scorpio', 'Water'), ('Leo', 'Fire'), ('Libra','Air');
+
+
+INSERT INTO zodiacs (name, type) VALUES('Taurus','Earth'), ('Scorpio', 'Water'), ('Leo', 'Fire'), ('Libra','Air');
