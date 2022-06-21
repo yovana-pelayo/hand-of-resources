4110239c1a23bf5713fb0e2b14d82e6f7d5ed4c6
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-21 00:31:27 -0700

    created a test that tests if adding an artist method works

--- __tests__/arists.test.js
+++ __tests__/arists.test.js
@@ -16,6 +16,19 @@ describe('artists routes', () => {
       { id: '3', name: 'Miley Cryus', genre: 'ROCk', hit: 'Midnight Sky' },
     ]);
   });
+  it(' POST /artists should add new god', async () => {
+    const resp = await request(app).post('/artists').send({
+      name: 'Maddie & Tae',
+      genre: 'country',
+      hit: 'Die From A Broken Heart',
+    });
+
+    expect(resp.status).toEqual(200);
+    expect(resp.body.name).toEqual('Yovana Pelayo');
+    expect(resp.body.genre).toEqual('country');
+    expect(resp.body.hit).toEqual('Die From A Broken Heart');
+    expect(resp.body.id).not.toBeUndefined();
+  });
   afterAll(() => {
     pool.end();
   });
